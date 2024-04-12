83fe3465bbb8cb4d533616f25998677e187410b2
Matheus Britto <55333133+nortan25@users.noreply.github.com>
2024-04-12 13:28:08 -0300

    Update credentials.php

--- db/credentials.php
+++ db/credentials.php
@@ -1,5 +1,5 @@
 <?php
-$api_url = "https://api-gsm.elbroos.com/";
-$username = "mpirlo25@gmail.com";
-$api_key = "5dbe39d22aead2feada7f887095aaadd";
+$api_url = "xxxx";
+$username = "xxxxx";
+$api_key = "xxxxx";
 ?>
